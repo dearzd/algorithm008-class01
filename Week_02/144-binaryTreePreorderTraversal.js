3023a47921ba44dcef185c61df2f82ff059f95dd
dearzd <1147433303@qq.com>
2020-04-27 00:06:54 +0800

    binary tree pre order traversla

--- Week_02/144-binaryTreePreorderTraversal.js
+++ Week_02/144-binaryTreePreorderTraversal.js
@@ -9,26 +9,18 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var inorderTraversal = function(root) {
+var preorderTraversal = function(root) {
     let values = [];
     traversal(root, values);
     return values;
-    /*if (!root) {
-        return [];
-    }
-    return [
-        ...inorderTraversal(root.left),
-        root.val,
-        ...inorderTraversal(root.right)
-    ];*/
 };
 
 var traversal = function(root, values) {
     if (!root) {
         return;
     }
-    traversal(root.left, values);
     values.push(root.val);
+    traversal(root.left, values);
     traversal(root.right, values);
     return values;
 };
